a8127735f0832cf8fb0e6a92dc5aa070df6b7bdd
Melissa Dumont <melissadumont91@gmail.com>
2019-04-09 17:11:49 -0400

    Finished skeleton tutorial of app

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 from random import randint
 
 app = Flask(__name__,
             static_folder="./dist/static",
             template_folder="./dist")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/api/random')
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,29 @@
 <template>
   <div>
     <p>Hello world! I'm in Vue.js.</p>
+    <p>Here's a random number: {{ randomNumber }}</p>
+    <button @click="getRandomFromBackend">New number?</button>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      randomNumber: 0
+    };
+  },
+  methods: {
+    getRandomFromBackend() {
+      axios
+        .get(`/api/random`)
+        .then(res => (this.randomNumber = res.data.randomNumber))
+        .catch(err => console.error(err));
+    }
+  },
+  created() {
+    this.getRandom();
+  }
+};
+</script>
