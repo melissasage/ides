76d316de12062c5457a4bbe912c7566f78ca511d
Melissa Dumont <melissadumont91@gmail.com>
2019-09-16 00:06:34 -0400

    Outlining

--- client/app.js
+++ client/app.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+// Start by writing here! Frontend first!
+
 const App = () => {
-  return <div>Lorem ipsum dolor sit amet</div>;
+  return (
+  <div>
+  <div>Schedule</div>
+  <div>Today
+    <div>9am - eat breakfast</div>
+    <div>12pm - eat lunch</div>
+    <div>6pm - eat dinner</div>
+  </div>
+  </div>
+  )
 };
 
 export default App;
